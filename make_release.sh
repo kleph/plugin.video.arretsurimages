fc851338fe544ced067e4bd22447860e92c3fa7e
kleph <kleph@kleph.eu>
2019-10-06 23:31:43 +0200

    Fix make_release.sh

--- make_release.sh
+++ make_release.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # create a .zip file ready for kodi
 
-PLUGIN_DIR="../$(basename ${PWD})"
+PLUGIN_DIR="$(basename ${PWD})"
 RELEASE_FILE="$(basename ${PWD}).zip"
 
 # remove existing release
 [ -f ${RELEASE_FILE} ] && rm ${RELEASE_FILE}
 
-zip -r "${RELEASE_FILE}" "${PLUGIN_DIR}/" -x "${PLUGIN_DIR}/.git/*" "${PLUGIN_DIR}/.idea/*" "${PLUGIN_DIR}/venv/*" "${PLUGIN_DIR}/.gitignore" "*.pyo"
+cd ..
+zip -r "${PLUGIN_DIR}/${RELEASE_FILE}" "${PLUGIN_DIR}/" -x "${PLUGIN_DIR}/.git/*" "${PLUGIN_DIR}/.idea/*" "${PLUGIN_DIR}/venv/*" "${PLUGIN_DIR}/.gitignore" "*.pyo"
 
+cd -
